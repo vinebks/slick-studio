0f08de65a8eaaa177e9e8b8b4875cc763b5d57e1
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 20:15:14 -0300

    feat: implements skeletons :sparkles:

--- src/components/Content/Main/index.tsx
+++ src/components/Content/Main/index.tsx
@@ -2,12 +2,14 @@ import { useCallback, useEffect, useState } from 'react';
 import { USERS } from '../../../constants/Users';
 import { formatDate } from '../../../utils/formatDate';
 import { MainContainer, Records, RawInfo } from './styles';
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { useTheme } from 'styled-components';
 
 export const Main: React.FC = () => {
   const [input, setInput] = useState('');
   const [filteredUsers, setFilteredUsers] = useState(USERS);
   const [isLoading, setIsLoading] = useState(true);
+  const { colors } = useTheme();
 
   const handleChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,59 +33,65 @@ export const Main: React.FC = () => {
   }, []);
 
   return (
-    <MainContainer>
-      <section>
-        <Records>
-          <h1>Total Records</h1>
-          <span>{USERS.length}</span>
-        </Records>
-        <RawInfo>
-          <span>Download Raw Data</span>
-          <input
-            type="search"
-            placeholder="It's working"
-            onChange={handleChange}
-          />
-        </RawInfo>
-      </section>
+    <SkeletonTheme color={colors.grey} highlightColor={colors.lightGrey}>
+      <MainContainer>
+        <section>
+          <Records>
+            <h1>Total Records</h1>
+            {isLoading ? (
+              <Skeleton circle height={50} width={50} duration={1} delay={1} />
+            ) : (
+              <span>{USERS.length}</span>
+            )}
+          </Records>
+          <RawInfo>
+            <span>Download Raw Data</span>
+            <input
+              type="search"
+              placeholder="It's working"
+              onChange={handleChange}
+            />
+          </RawInfo>
+        </section>
 
-      <table>
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Source</th>
-            <th>Time Stamp</th>
-            <th>Error Flag</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
+        <table>
+          <thead>
             <tr>
-              <td>
-                <Skeleton />
-              </td>
-              <td>
-                <Skeleton />
-              </td>
-              <td>
-                <Skeleton />
-              </td>
-              <td>
-                <Skeleton />
-              </td>
+              <th>User</th>
+              <th>Source</th>
+              <th>Time Stamp</th>
+              <th>Error Flag</th>
             </tr>
-          ) : (
-            filteredUsers.map((user, key) => (
-              <tr key={key}>
-                <td>{user.name}</td>
-                <td>{user.source}</td>
-                <td>{formatDate(user.timeStamp)}</td>
-                <td>{user.errorFlag}</td>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <tr>
+                <td>
+                  <Skeleton />
+                </td>
+                <td>
+                  <Skeleton />
+                </td>
+                <td>
+                  <Skeleton />
+                </td>
+                <td>
+                  <Skeleton />
+                </td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-    </MainContainer>
+            ) : (
+              filteredUsers.map((user, key) => (
+                <tr key={key}>
+                  <td>{user.name}</td>
+                  <td>{user.source}</td>
+                  <td>{formatDate(user.timeStamp)}</td>
+                  <td>{user.errorFlag}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </MainContainer>
+    </SkeletonTheme>
   );
 };
