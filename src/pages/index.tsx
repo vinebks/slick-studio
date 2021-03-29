38fa1cb3b0f07de0a7377df17822335eb4508880
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 14:05:10 -0300

    chore: testing theme :lipstick:

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.green};
+  font-size: 50px;
+`;
+
+export default function Home() {
+  return <Title>My page</Title>;
+}
