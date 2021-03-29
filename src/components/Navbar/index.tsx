8a7909664690d4aa74aa84de2adf17282020a570
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 16:30:56 -0300

    feat: active item :sparkles:

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,25 @@
 import { NavbarContainer } from './styles';
+import { useCallback, useState } from 'react';
+import { NAVBAR_LIST } from '../../constants/NavbarList';
 
 export const Navbar: React.FC = () => {
+  const [active, setActive] = useState('Dashboard');
+
+  const handleActive = useCallback((item: string) => {
+    setActive(item);
+  }, []);
+
   return (
     <NavbarContainer>
       <ul>
-        <li className="active">Dashboard</li>
-        <li>Analytics</li>
-        <li>Reports</li>
-        <li>Settings</li>
+        {NAVBAR_LIST.map((item) => (
+          <li
+            onClick={() => handleActive(item)}
+            className={active === item ? 'active' : ''}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
       <div>
         <span>Welcome, Felipe</span>
