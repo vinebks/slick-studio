7607e5d7aeab8b552ec77805b1858abb318a81c1
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 19:57:55 -0300

    feat: filtering user list :sparkles:

--- src/components/Content/Main/index.tsx
+++ src/components/Content/Main/index.tsx
@@ -1,18 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
 import { USERS } from '../../../constants/Users';
 import { formatDate } from '../../../utils/formatDate';
 import { MainContainer, Records, RawInfo } from './styles';
+import Skeleton from 'react-loading-skeleton';
 
 export const Main: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(USERS);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setIsLoading((loading) => !loading);
+      if (event.target.value === '') {
+        setFilteredUsers(USERS);
+        setIsLoading((loading) => !loading);
+      } else {
+        setInput(event.target.value);
+        setFilteredUsers(USERS.filter((user) => user.name.includes(input)));
+        setIsLoading((loading) => !loading);
+      }
+    },
+    [input]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading((prevState) => !prevState);
+    }, 1000);
+  }, []);
+
   return (
     <MainContainer>
       <section>
         <Records>
           <h1>Total Records</h1>
-          <span>1</span>
+          <span>{USERS.length}</span>
         </Records>
         <RawInfo>
           <span>Download Raw Data</span>
-          <input type="search" placeholder="Search" />
+          <input
+            type="search"
+            placeholder="It's working"
+            onChange={handleChange}
+          />
         </RawInfo>
       </section>
 
@@ -26,14 +57,31 @@ export const Main: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {USERS.map((user) => (
+          {isLoading ? (
             <tr>
-              <td>{user.name}</td>
-              <td>{user.source}</td>
-              <td>{formatDate(user.timeStamp)}</td>
-              <td>{user.errorFlag}</td>
+              <td>
+                <Skeleton />
+              </td>
+              <td>
+                <Skeleton />
+              </td>
+              <td>
+                <Skeleton />
+              </td>
+              <td>
+                <Skeleton />
+              </td>
             </tr>
-          ))}
+          ) : (
+            filteredUsers.map((user, key) => (
+              <tr key={key}>
+                <td>{user.name}</td>
+                <td>{user.source}</td>
+                <td>{formatDate(user.timeStamp)}</td>
+                <td>{user.errorFlag}</td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </MainContainer>
