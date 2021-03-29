b55982d2239c9530458519ee8d2769d72f0b2f3b
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 15:09:04 -0300

    feat: logo component :sparkles:

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { LogoContainer } from './styles';
+
+export const Logo: React.FC = () => {
+  return (
+    <LogoContainer>
+      <h1>Slick Studio</h1>
+    </LogoContainer>
+  );
+};
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+export const LogoContainer = styled.section`
+  ${({ theme }) => css`
+    grid-area: Logo;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding-left: 1.5rem;
+
+    border-right: 1px solid ${theme.colors.grey};
+    border-left: 1px solid ${theme.colors.grey};
+    border-bottom: 1px solid ${theme.colors.grey};
+  `}
+`;
