8c4dc2e38fed865d57166e942071a353b841c579
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 18:55:47 -0300

    feat: new constant user :sparkles:

---
+++ src/constants/Users.ts
@@ -0,0 +1,38 @@
+export const USERS = [
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+  {
+    name: 'Felipe',
+    source: 'Dispatch',
+    timeStamp: Date.now(),
+    errorFlag: 0,
+  },
+];
