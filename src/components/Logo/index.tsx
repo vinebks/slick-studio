434fad70e120da83037b55af741a8f684dee4c89
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 20:40:20 -0300

    build: deploy :construction_worker:

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { LogoContainer } from './styles';
 export const Logo: React.FC = () => {
   return (
     <LogoContainer>
-      <h1>Slick Studio</h1>
+      <h1>Slick Studio </h1>
     </LogoContainer>
   );
 };
