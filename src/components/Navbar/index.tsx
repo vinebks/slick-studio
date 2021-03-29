09a80c347ffb0560b61e682d5133b3a800249d25
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-29 15:24:23 -0300

    feat: add navbar items and avatar :sparkles:

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,21 @@
 import { NavbarContainer } from './styles';
 
 export const Navbar: React.FC = () => {
-  return <NavbarContainer>Navbar</NavbarContainer>;
+  return (
+    <NavbarContainer>
+      <ul>
+        <li>Dashboard</li>
+        <li>Analytics</li>
+        <li>Reports</li>
+        <li>Settings</li>
+      </ul>
+      <div>
+        <span>Welcome, Felipe</span>
+        <img
+          src="https://avatars.githubusercontent.com/u/43390533?v=4"
+          alt="felipe github"
+        />
+      </div>
+    </NavbarContainer>
+  );
 };
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -2,5 +2,42 @@ import styled from 'styled-components';
 
 export const NavbarContainer = styled.nav`
   grid-area: Navbar;
-  background: blue;
+
+  display: flex;
+  justify-content: space-around;
+
+  ul {
+    display: flex;
+    align-items: center;
+
+    li + li {
+      margin-left: 2rem;
+    }
+
+    li {
+      transition: color 0.2s;
+      &:hover {
+        cursor: pointer;
+        color: ${({ theme }) => theme.colors.blue};
+      }
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    img {
+      border-radius: 50%;
+      max-width: 3rem;
+      margin-left: 1rem;
+
+      transition: transform 0.2s;
+
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.1);
+      }
+    }
+  }
 `;
